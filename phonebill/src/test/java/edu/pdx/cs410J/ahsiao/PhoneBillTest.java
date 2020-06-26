603b9dccd995a1282ba6804ce52201597d11d7f1
AustenHsiao <Austenhsiao@gmail.com>
2020-06-26 14:36:47 -0700

    additional phonebilltest

--- phonebill/src/test/java/edu/pdx/cs410J/ahsiao/PhoneBillTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/ahsiao/PhoneBillTest.java
@@ -1,6 +1,8 @@
 package edu.pdx.cs410J.ahsiao;
 import org.junit.Test;
 
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 
 public class PhoneBillTest {
@@ -11,4 +13,19 @@ public class PhoneBillTest {
         assertEquals(dummy.getCustomer(), "Bob");
     }
 
+    @Test public void addingPhoneCall(){
+        PhoneCall sampleCall = new PhoneCall("Bob",
+                "503-111-1111",
+                "503-222-2222",
+                "06/26/2020",
+                "12:00",
+                "6/27/2020",
+                "19:01");
+        PhoneBill dummy = new PhoneBill("Bob", sampleCall);
+
+        // We only have one phone call for Project1
+        for(Object onePhoneCall: dummy.getPhoneCalls()){
+            assertEquals(onePhoneCall, sampleCall);
+        }
+    }
 }
