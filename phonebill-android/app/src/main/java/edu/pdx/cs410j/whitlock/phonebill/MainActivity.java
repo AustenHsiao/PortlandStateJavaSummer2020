382a917dab79ccac47328932485392e2bf770999
AustenHsiao <Austenhsiao@gmail.com>
2020-08-05 00:22:57 -0700

    starting point for android

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/MainActivity.java
@@ -21,15 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -52,5 +43,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void readme(View v){
+
+    }
+
+    public void addPhoneBill(View v){
+
     }
 }
